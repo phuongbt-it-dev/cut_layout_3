2fe74d8e3eb55490ddc0d90e6c457d958e0dc324
phuongbt-it-dev <phuong.it.dev@gmail.com>
2018-08-23 20:01:10 +0700

    add source

--- dist/js/main.js
+++ dist/js/main.js
@@ -35,29 +35,19 @@ $(function () {
 $(document).ready(function () {
 	// Phần Slider
 	$('.home-slider .owl-carousel').owlCarousel({
-		items: 1,
-		nav: false,
-		dots: true,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>']
-	});
-	// Phần Clients
-	$('.home-clients .owl-carousel').owlCarousel({
-		items: 1,
+		loop: true,
+		margin: 10,
 		nav: true,
-		dots: false,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		item: 1,
 		responsive: {
-			// breakpoint from 480 up
-			480: {
-				items: 2
+			0: {
+				items: 1
 			},
-			// breakpoint from 768 up
 			768: {
-				items: 4
+				items: 1
 			},
-			// breakpoint from 992 up
-			992: {
-				items: 6
+			1024: {
+				items: 1
 			}
 		}
 	});
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,30 +2,20 @@
 $(document).ready(function () {
 	// Phần Slider
 	$('.home-slider .owl-carousel').owlCarousel({
-		items: 1,
-		nav: false,
-		dots: true,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-	});
-	// Phần Clients
-	$('.home-clients .owl-carousel').owlCarousel({
-		items: 1,
-		nav: true,
-		dots: false,
-		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
-		responsive: {
-			// breakpoint from 480 up
-			480: {
-				items: 2,
+		loop:true,
+		margin:10,
+		nav:true,
+		item:1,
+		responsive:{
+			0:{
+				items:1
 			},
-			// breakpoint from 768 up
-			768: {
-				items: 4,
+			768:{
+				items:1
 			},
-			// breakpoint from 992 up
-			992: {
-				items: 6,
+			1024:{
+				items:1
 			}
 		}
-	});
+	})
 });
